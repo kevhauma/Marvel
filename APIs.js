9ed02cbedd45e50578efd55a787b171341b1bb9f
kevin hauman <hauman.kevin@hotmail.com>
2017-06-06 18:26:38 +0200

    added style by CowSaysWoof and added characterlists

--- APIs.js
+++ APIs.js
@@ -1,14 +1,27 @@
 var App = angular.module('PrikbordApp', []);
       App.controller('MainController', function($scope,$window, $http) {
-        $http.get('http://gateway.marvel.com/v1/public/series?ts=1&apikey=580ec40a7f1d18e71b191f04a401d704&hash=4ae65965f96b46fcdf6425f23f5198d7')
-        .then(function(response){
+          $scope.limit = 10;
+          $scope.startswith = 'a';
+          $scope.GetSeries = function(){
+            $http.get('http://gateway.marvel.com/v1/public/series?ts=1&apikey=580ec40a7f1d18e71b191f04a401d704&hash=4ae65965f96b46fcdf6425f23f5198d7&titleStartsWith=' + $scope.startswith)
+            .then(function(response){
             $scope.serieslist = response.data.data.results;
-        });
+            });
+          };
         $scope.GetComics = function(){
-          $http.get('http://gateway.marvel.com/v1/public/series/' + $scope.selectedseries + '/comics?ts=1&apikey=580ec40a7f1d18e71b191f04a401d704&hash=4ae65965f96b46fcdf6425f23f5198d7')
+          $http.get('http://gateway.marvel.com/v1/public/series/' + $scope.selectedseries + '/comics?ts=1&apikey=580ec40a7f1d18e71b191f04a401d704&hash=4ae65965f96b46fcdf6425f23f5198d7&limit=' + $scope.limit + '&orderBy=-title')
           .then(function(response){
               $scope.comiclist = response.data.data.results;
           });
-        }
+        };
+        $scope.ShowCharacters = function(comicid){
+            $http.get('http://gateway.marvel.com/v1/public/comics/' + comicid + '/characters?ts=1&apikey=580ec40a7f1d18e71b191f04a401d704&hash=4ae65965f96b46fcdf6425f23f5198d7')
+            .then(function(response){
+            $scope.characterlist = response.data.data.results;
+                })
+            .then(function(){
+                document.getElementById(comicid).style.visibility = visibile;
+            });
+        };
       });
       
